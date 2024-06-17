c13f3343ac7752b35a3d93ef213018a2d96e8c40
duberii <richard.dube@uconn.edu>
2024-06-17 13:16:59 -0400

    Added separate makefile for unit tests

---
+++ UnitTests/compare_likelihoodsMPI.C
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <fstream>
+#include <complex>
+#include <string>
+#include <vector>
+#include <utility>
+#include <map>
+#include <mpi.h>
+#include "IUAmpTools/ConfigFileParser.h"
+#include "IUAmpTools/ConfigurationInfo.h"
+#include "IUAmpToolsMPI/AmpToolsInterfaceMPI.h"
+#include "IUAmpToolsMPI/DataReaderMPI.h"
+#include "DalitzDataIO/DalitzDataReader.h"
+#include "DalitzAmp/BreitWigner.h"
+#include "DalitzAmp/Constraint.h"
+
+#include "IUAmpTools/report.h"
+static const char* kModule = "fitAmplitudesMPI";
+
+using std::complex;
+using namespace std;
+
+int main( int argc, char* argv[] ){
+
+  MPI_Init( &argc, &argv );
+  
+  int rank;
+  int size;
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &size );
+
+
+    // ************************
+    // usage
+    // ************************
+
+
+  if (argc <= 1){
+    report( INFO, kModule ) << "Usage:" << endl << endl;
+    report( INFO, kModule ) << "\tfitAmplitudesMPI <config file name>" << endl << endl;
+    MPI_Finalize();
+    return 0;
+  }
+
+  report( INFO, kModule ) << " *** Performing the Fit *** " << endl;
+
+
+    // ************************
+    // parse the command line parameters
+    // ************************
+
+  string cfgname(argv[1]);
+
+  report( INFO, kModule ) << "Config file name:  " << cfgname << endl << endl;
+
+
+    // ************************
+    // parse the config file
+    // ************************
+
+  ConfigFileParser parser(cfgname);
+  ConfigurationInfo* cfgInfo = parser.getConfigurationInfo();
+  cfgInfo->display();
+
+
+    // ************************
+    // AmpToolsInterface
+    // ************************
+
+  AmpToolsInterfaceMPI::registerAmplitude(BreitWigner());
+  AmpToolsInterfaceMPI::registerNeg2LnLikContrib(Constraint());
+  AmpToolsInterfaceMPI::registerDataReader(DataReaderMPI<DalitzDataReader>());
+
+  AmpToolsInterfaceMPI ATI(cfgInfo);
+
+  if (rank == 0){
+    
+    double neg2LL_before = ATI.likelihood();
+    MinuitMinimizationManager* fitManager = ATI.minuitMinimizationManager();
+    fitManager->setStrategy(1);
+
+    fitManager->migradMinimization();
+
+    if( fitManager->status() != 0 && fitManager->eMatrixStatus() != 3 ){
+      report( WARNING, kModule ) << "Fit failed." << endl;
+    }
+
+    double neg2LL_after = ATI.likelihood();
+
+    ATI.finalizeFit();
+
+    if (cfgname == "dalitz1.cfg") {
+      assert(neg2LL_before == 30795.5);
+      assert(neg2LL_after == -2.07478e+03);
+    } else if (cfgname == "dalitz2.cfg") {
+      assert(neg2LL_before == 66474.2);
+      assert(neg2LL_after == -5.54876e+03);
+    } else if (cfgname == "dalitz3.cfg") {
+      assert(neg2LL_before == 30795.5);
+      assert(neg2LL_after == -2.07478e+03);
+    }
+  }
+
+
+  ATI.exitMPI();
+  MPI_Finalize();
+
+  return 0;
+
+}
