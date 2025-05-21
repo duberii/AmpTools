0bb8b994f2a1dd3b031a913cf85904a34b6c2b77
duberii <108533494+duberii@users.noreply.github.com>
2025-05-21 12:01:25 -0400

    Update fitResultsTestMPI.cc

--- UnitTests/fitResultsTestMPI.cc
+++ UnitTests/fitResultsTestMPI.cc
@@ -86,10 +86,10 @@ bool testFitResults(const FitResults* fitResults) {
     fin >> num_parameters;
     unit_test.add(abs(intensity_first-fitResults->intensity().first)<= 1, "Intensity matches model");
     unit_test.add(abs(intensity_second-fitResults->intensity().second)<= 1e1, "Intensity error matches model");
-    cout << "______________________________________" << endl;
-    cout << to_string(intensity_first) << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << to_string(intensity_second) << endl;
     cout << to_string(fitResults->intensity().second) << endl;
-    cout << "__________________________________________________" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     unit_test.add(abs(pd_first-fitResults->phaseDiff("base::s1::R12","base::s1::R13").first)<= 1e-03, "Phase difference between amplitudes matches model");
     unit_test.add(abs(pd_second-fitResults->phaseDiff("base::s1::R12","base::s1::R13").second)<= 1, "Phase difference error between amplitudes matches model");
     unit_test.add(abs(ppBase_real-fitResults->productionParameter("base::s1::R12").real())<= 1e-03, "Real part of base reaction production parameter matches model");
